9f093db9944d77b635b3e01db2385920992aaa2b
Feroz Rana <rana.csedu@gmail.com>
2018-06-25 17:59:50 -0700

    Appsflyer app demo

---
+++ AppsFlyerDemo/app/src/main/java/com/example/teppei/appsflyerdemo/HomeActivity.java
@@ -0,0 +1,102 @@
+package com.example.teppei.appsflyerdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.appsflyer.AppsFlyerConversionListener;
+import com.appsflyer.AppsFlyerLib;
+
+import java.util.Map;
+
+public class HomeActivity extends AppCompatActivity {
+    private static final String AF_DEV_KEY = "<HLSoWZFpzn84UNyH8kWFpN>";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // initizlize AppsFlyer object
+        AppsFlyerConversionListener conversionDataListener =
+                new AppsFlyerConversionListener() {
+                    @Override
+                    public void onInstallConversionDataLoaded(Map<String, String> conversionData) {
+
+                    }
+
+                    @Override
+                    public void onInstallConversionFailure(String errorMessage) {
+
+                    }
+
+                    @Override
+                    public void onAppOpenAttribution(Map<String, String> attributionData) {
+
+                    }
+
+                    @Override
+                    public void onAttributionFailure(String errorMessage) {
+
+                    }
+                    //
+                };
+        //AppsFlyerLib.getInstance().init(AF_DEV_KEY, conversionDataListener, getApplicationContext());
+        //AppsFlyerLib.getInstance().startTracking(this);
+        AppsFlyerLib.getInstance().startTracking(this.getApplication(),AF_DEV_KEY);
+        AppsFlyerLib.getInstance().sendDeepLinkData(this);
+
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Home"));
+        tabLayout.addTab(tabLayout.newTab().setText("Product"));
+        tabLayout.addTab(tabLayout.newTab().setText("About"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        final PagerAdapter adapter = new PagerAdapter
+                (getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+}
---
+++ AppsFlyerDemo/app/src/main/java/com/example/teppei/appsflyerdemo/HomeFragment.java
@@ -0,0 +1,41 @@
+package com.example.teppei.appsflyerdemo;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    private WebView webview;
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        webview =(WebView)view.findViewById(R.id.webView);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setDomStorageEnabled(true);
+        webview.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webview.loadUrl("https://www.appsflyer.com/");
+
+        return view;
+    }
+
+}
